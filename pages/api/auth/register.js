643299be6f14e850e1ab3661b2262545b7914531
azzahra-jateng <azzahra.jateng@gmail.com>
2021-04-16 14:39:22 +0700

    make register

---
+++ pages/api/auth/register.js
@@ -0,0 +1,21 @@
+import db from '../../../libs/db';
+import bcrypt from 'bcryptjs';
+
+export default async function hanlder(req, res){
+    if(req.method !== 'POST') return res.status(405).end();
+    const {email , password} = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const passwordHash = bcrypt.hashSync(password , salt);
+    const register = await db('users').insert({
+        email,
+        password: passwordHash
+    });
+    const resgiterUser = await db('users')
+                                .where({id: register})
+                                .first();
+    res.status(200);
+    res.json({
+        message: 'register success',
+        data: resgiterUser
+    })
+}
