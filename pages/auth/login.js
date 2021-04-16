cea0b8d0ad1a4f24c81b197cde3ee6f19d24485b
youone04 <yudi.gunaone87@gmail.com>
2021-04-16 22:45:15 +0700

    make page login

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,51 +1,51 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
-const Register = () => {
-    const [fileds , setFileds] = useState({
+const Login = () => {
+    const[fileds , setFileds] = useState({
         email: '',
         password: ''
-    });
-
-    const handleSubmit = async(e) => {
+    })
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // console.log(fileds)
-        const register = await fetch('/api/auth/register' ,{
+        const login  = await fetch('/api/auth/login', {
             method: 'POST',
             body: JSON.stringify(fileds),
             headers: {
                 'Content-Type': 'application/json'
             }
         });
-        if(!register.ok) return;
-        const registerRe = await register.json();
-        console.log(registerRe)
-        
-       
-    }
+        if(!login.ok) return;
+       const loginRes = await login.json();
+       console.log(loginRes)
+       if(loginRes){
+        localStorage.setItem('dataLogin' , JSON.stringify(loginRes));
+       }
 
-    const hanldeChange = (e) =>{
+    }
+    const hanldeChange = (e) => {
         const name = e.target.getAttribute('name');
         setFileds({
             ...fileds,
             [name]: e.target.value
         })
 
+
     }
 
     return(
         <div>
-            <h1>Register Page</h1>
+            <h1>Login Page</h1>
 
             <div>
                 <form onSubmit={handleSubmit.bind(this)}>
                     <input type="text" name="email" placeholder="Email" onChange={hanldeChange.bind(this)} /> <br/>
                     <input type="password" name="password" placeholder="Password" onChange={hanldeChange.bind(this)}  /> <br/>
                     <button type="submit">
-                        Register
+                        Login
                     </button>
                 </form>
             </div>
         </div>
     )
 }
-export default Register;
+export default Login;
