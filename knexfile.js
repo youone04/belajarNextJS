11930dcc5705264a164c4cef59f3224d80672c71
youone04 <yudi.gunaone87@gmail.com>
2021-04-18 02:21:05 +0700

    remove fil knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,12 @@
 module.exports = {
 
   development: {
-    client: 'mysql',
+    client: process.env.DB_CLIENT,
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: '',
-      database:'db_belajarnext'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
   }
   },
 
